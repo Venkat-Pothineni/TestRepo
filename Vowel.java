0548dbc493bc88cd71f80517e8fd65346fc37325
Venkateswara Rao Pothineni <89446550+Venkat-Pothineni@users.noreply.github.com>
2026-03-03 10:15:04 +0530

    Refactor Main class to Vowel class
    Did run in online compiler first...

--- Vowel.java
+++ Vowel.java
@@ -1,11 +1,12 @@
 /* String starts with vowel & end with vowel */
 import java.util.*;
 import java.util.stream.Collectors;
-class Main {
+class Vowel {
     public static void main(String[] args) {
         System.out.println("Try programiz.pro");
         List<String> li = List.of("america", "ireland","africa", "england", "asia", "Apple", "ideA");
         li = li.stream().filter(e-> e.matches("^[AEIOUaeiou].*[aeiouAEIOU]$")).collect(Collectors.toList());
         li.forEach(System.out::println);
     }
+
 }
